cc1a8601115080181ce5992850a55a675af2cc8b
Melody Wang <melody26613@gmail.com>
2022-11-27 11:49:57 +0800

    change to use view binding

--- fat_aar/fataar/src/main/java/com/example/fataar/FatAar.java
+++ fat_aar/fataar/src/main/java/com/example/fataar/FatAar.java
@@ -1,47 +1,36 @@
 package com.example.fataar;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
+import com.example.fataar.databinding.LayoutFatAarBinding;
 import com.example.subaar1.SubAar1;
 import com.example.subaar2.SubAar2;
 
 public class FatAar {
-    private final static String TAG = FatAar.class.getSimpleName();
-
-    private final FrameLayout rootLayout;
+    private final LayoutFatAarBinding binding;
     private final Context context;
 
     public FatAar(@NonNull FrameLayout layout) {
-        rootLayout = layout;
-        context = rootLayout.getContext();
+        this.context = layout.getContext();
+        this.binding = LayoutFatAarBinding.inflate(LayoutInflater.from(context), layout, true);
 
         initLayout();
     }
 
     private void initLayout() {
-        ConstraintLayout layout = (ConstraintLayout) LayoutInflater.from(context).inflate(
-                R.layout.layout_fat_aar_sample,
-                rootLayout,
-                false
-        );
-
         SubAar1 subAar1 = new SubAar1(context);
         SubAar2 subAar2 = new SubAar2(context);
 
-        TextView textView1 = layout.findViewById(R.id.text1);
-        TextView textView2 = layout.findViewById(R.id.text2);
+        TextView textView1 = binding.text1;
+        TextView textView2 = binding.text2;
 
         textView1.setText(subAar1.getText());
         textView2.setText(subAar2.getText());
-
-        rootLayout.addView(layout);
     }
 
 }
--- sample_app/app/src/main/java/com/example/fataarsampleapp/MainActivity.java
+++ sample_app/app/src/main/java/com/example/fataarsampleapp/MainActivity.java
@@ -3,18 +3,22 @@ package com.example.fataarsampleapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.widget.FrameLayout;
 
 import com.example.fataar.FatAar;
+import com.example.fataarsampleapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-        FrameLayout layout = findViewById(R.id.layoutForFatAar);
-        FatAar fatAar = new FatAar(layout);
-    }
+		ActivityMainBinding binding = ActivityMainBinding.inflate(LayoutInflater.from(MainActivity.this));
+		setContentView(binding.getRoot());
+
+		FrameLayout layout = binding.layoutForFatAar;
+		FatAar fatAar = new FatAar(layout);
+	}
 }
